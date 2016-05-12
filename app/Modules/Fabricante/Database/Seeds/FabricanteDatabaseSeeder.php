49fe2d0fbf906e51ea2db9534f726537eb7a2a09
Eddie Rada <eddierada@gmail.com>
2016-05-12 11:11:18 -0500

    Alimentar tabla de fabricantes

--- app/Modules/Fabricante/Database/Seeds/FabricanteDatabaseSeeder.php
+++ app/Modules/Fabricante/Database/Seeds/FabricanteDatabaseSeeder.php
@@ -11,11 +11,32 @@ class FabricanteDatabaseSeeder extends Seeder
 	 *
 	 * @return void
 	 */
+
+
 	public function run()
 	{
-		Model::unguard();
+		// Model::unguard();
 
 		// $this->call('App\Modules\Fabricante\Database\Seeds\FoobarTableSeeder');
+
+
+		$faker = Faker::create();
+		for ($i=0; $i < 50; $i++) {
+			\DB::table('fabricantes')->insert(array(
+				'nombre' => $faker->firstNameFemale,
+				'direccion' => $faker->randomElement(['Calle 45 # 54 -998','Calle 54 # 45 -87','Carrera 97 # 2 - 104']),
+				'telefono' 	=> $faker->phoneNumber,
+				'created_at' => date('Y-m-d H:m:s'),
+				'updated_at' => date('Y-m-d H:m:s')
+			));
+		}
+
+
+
 	}
+
+
+
+
 
 }
