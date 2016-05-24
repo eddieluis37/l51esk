78e3f79e8abbcafa96e6056c44c2d74408ebf955
Eddie Rada <eddierada@gmail.com>
2016-05-24 11:10:58 -0500

    1 Refactorización

--- app/Modules/Ticket/Database/Migrations/2016_05_18_194611_create_all_tickets_tables.php
+++ app/Modules/Ticket/Database/Migrations/2016_05_18_194611_create_all_tickets_tables.php
@@ -12,41 +12,115 @@ class CreateAllTicketsTables extends Migration
 	 */
 	public function up()
 	{
+		// Crea la tabla importances
+
+		Schema::create('importances', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name', 100)->unique();
+
+
+		});
+
+		// Crea la tabla types
+
+		Schema::create('types', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name', 100)->unique();
+
+
+		});
+
+
 		//Crea la tabla para almacenar tickets
 
 		Schema::create('tickets', function (Blueprint $table) {
 			$table->increments('id');
 			$table->string('name', 100)->unique();
 			$table->text('text')->nullable();
-            $table->string('description')->nullable();
-            $table->unsignedInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')
-                ->onUpdate('cascade')->onDelete('cascade');
-            $table->timestamps();
-            $table->softDeletes();
+			$table->string('description')->nullable();
+			$table->unsignedInteger('user_id');
+			$table->foreign('user_id')->references('id')->on('users')
+				->onUpdate('cascade')->onDelete('cascade');
+
+			$table->unsignedInteger('importance_id');
+			$table->foreign('importance_id')->references('id')->on('importances')
+				->onUpdate('cascade')->onDelete('cascade');
 
 
-        });
+			$table->unsignedInteger('type_id');
+			$table->foreign('type_id')->references('id')->on('types')
+				->onUpdate('cascade')->onDelete('cascade');
 
-        //Crea la tabla para relacionar ticket con los usuarios
 
-        Schema::create('ticket_user', function(Blueprint $table) {
+			$table->timestamps();
+			$table->softDeletes();
 
-            $table->unsignedInteger('user_id');
-            $table->unsignedInteger('ticket_id');
 
-            $table->foreign('user_id')->references('id')->on('users')
-                ->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('ticket_id')->references('id')->on('tickets')
-                ->onUpdate('cascade')->onDelete('cascade');
+		});
 
-            $table->primary(['user_id', 'ticket_id']);
+		/**
+		 *
+		 * Crea la tabla para relacionar ticket con los usuarios
+		 *
+		 * Schema::create('ticket_user', function(Blueprint $table) {
+		 *
+		 * $table->unsignedInteger('user_id');
+		 * $table->unsignedInteger('ticket_id');
+		 *
+		 * $table->foreign('user_id')->references('id')->on('users')
+		 * ->onUpdate('cascade')->onDelete('cascade');
+		 * $table->foreign('ticket_id')->references('id')->on('tickets')
+		 * ->onUpdate('cascade')->onDelete('cascade');
+		 *
+		 * $table->primary(['user_id', 'ticket_id']);
+		 *
+		 * $table->timestamps();
+		 *
+		 * });
+		 */
 
-            $table->timestamps();
+		//Crea la tabla para almacenar notas
 
-        });
+		Schema::create('notes', function (Blueprint $table) {
+			$table->increments('id');
+			$table->text('text')->nullable();
+
+			$table->unsignedInteger('user_id');
+			$table->foreign('user_id')->references('id')->on('users')
+				->onUpdate('cascade')->onDelete('cascade');
+
+
+			$table->unsignedInteger('ticket_id');
+			$table->foreign('ticket_id')->references('id')->on('tickets')
+				->onUpdate('cascade')->onDelete('cascade');
+
+			$table->timestamps();
+			$table->softDeletes();
+
+
+		});
+
+		Schema::create('watchers', function (Blueprint $table) {
+			$table->increments('id');
+
+			$table->unsignedInteger('user_id');
+			$table->foreign('user_id')->references('id')->on('users')
+				->onUpdate('cascade')->onDelete('cascade');
+
+
+			$table->unsignedInteger('ticket_id');
+			$table->foreign('ticket_id')->references('id')->on('tickets')
+				->onUpdate('cascade')->onDelete('cascade');
+
+			$table->timestamps();
+			$table->softDeletes();
+
+
+		});
+
 
 	}
+
 
 	/**
 	 * Reverse the migrations.
@@ -56,6 +130,10 @@ class CreateAllTicketsTables extends Migration
 	public function down()
 	{
 		Schema::dropIfExists('tickets');
-        Schema::dropIfExists('ticket_user');
+		Schema::dropIfExists('importances');
+		Schema::dropIfExists('types');
+		Schema::dropIfExists('notes');
+		Schema::dropIfExists('watchers');
 	}
+
 }
