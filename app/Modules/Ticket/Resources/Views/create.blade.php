d41830862c5128e55bb7c54aa83c927b86b21ee9
Eddie Rada <eddierada@gmail.com>
2016-05-24 12:02:54 -0500

    1 Refactorización

--- app/Modules/Ticket/Resources/Views/create.blade.php
+++ app/Modules/Ticket/Resources/Views/create.blade.php
@@ -4,9 +4,8 @@
 @endsection
 
 @section('content')
-    {{ csrf_field() }}
     <div class='row'>
-        <div class='col-md-3'>
+        <div class='col-md-auto'>
             <div class="box box-primary">
                 <div class="box-header with-border">
                     <h3 class="box-title">{{ trans('ticket::general.page.method-name.box-title') }}</h3>
@@ -17,12 +16,42 @@
                     </div>
                 </div>
                 <div class="box-body">
+                    <table class="table table-bordered" id="MyTable">
+                        <thead>
+                        <tr>
+                            <th class="text-center">ID</th>
+                            <th class="text-center">Tiquet</th>
+                            <th class="text-center">Text</th>
+                            <th class="text-center">Descripción</th>
+                            <th class="text-center">Acciones</th>
+                        </tr>
+                        </thead>
+                        <tbody>
 
-                    {!! Form::open([ 'route' => 'ticket.store', 'method' => 'POST']) !!}
-                    @include('ticket::fields')
-                    <button type="submit" class="btn btn-success btn-block">Guardar</button>
-                    {!! Form::close() !!}
+                        @foreach($tickets as $ticket)
+                            <tr>
+                                <td class="text-center">{{ $ticket->id }}</td>
+                                <td class="text-center">{{ $ticket->name }}</td>
+                                <td class="text-center">{{ $ticket->text }}</td>
+                                <td class="text-center">{{ $ticket->description }}</td>
 
+
+
+                                <td class="text-center">
+                                    <button type="submit" class="btn btn-danger btn-xs">
+                                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                                    </button>
+                                    <a href="{{ url('/ticket/'.$ticket->id.'/edit') }}" class="btn btn-info btn-xs">
+                                        <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                                    </a>
+                                </td>
+
+                                {!! Form::close() !!}
+
+                            </tr>
+                        @endforeach
+
+                    </table>
 
                 </div><!-- /.form-group -->
 
